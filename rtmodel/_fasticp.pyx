b7ca5b58f5bac98846df72c2ecbd7cc113d362f4
Andrew Miller <amiller@dappervision.com>
2012-12-20 15:55:13 -0500

    Checked in a fix to FastICP, bottom triangle of the accumulator matrix
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- rtmodel/_fasticp.pyx
+++ rtmodel/_fasticp.pyx
@@ -21,41 +21,43 @@ cdef _c_fast_icp(np.float32_t *xyzA,
                  np.uint8_t *maskA,
                  int height, int width,
                  np.float32_t *xyzB,
+                 np.float32_t *normB,
                  int length,
                  np.float32_t *uv,
-                 np.float32_t *matB,
-                 np.float32_t *matKKBtoA,
-                 float rate, float dist2,
+                 np.float32_t *matKK,
+                 float dist2,
+                 float normthresh,
                  np.float32_t A[6][6],
                  np.float32_t B[6]):
 
     cdef int i, _blank
-    cdef float x, y, z, uw, vw, w, d, zz
+    cdef float uw, vw, w, d, zz, normdot
     cdef int u, v
     cdef float xA, yA, zA
     cdef float nxA, nyA, nzA
+    cdef float nxB, nyB, nzB
     cdef float xB, yB, zB
     cdef float c[3]
-    cdef float err = 0.
+    cdef float sse = 0.
     cdef int npairs = 0
 
-    # Sample from the set of points A
-    if rate <= 0.: rate = 0.001
-    cdef float two_over_rate = 2.0 / rate
 
-    i = 0
-    while True:
-        i += int(1 + two_over_rate * drand48())
-        if not i < length:
-            break
+    for i in range(length):
 
-        # Get the coordinates in the range image A
-        x = xyzB[i*3+0];
-        y = xyzB[i*3+1];
-        z = xyzB[i*3+2];
-        uw = matKKBtoA[ 0]*x + matKKBtoA[ 1]*y + matKKBtoA[ 2]*z + matKKBtoA[ 3];
-        vw = matKKBtoA[ 4]*x + matKKBtoA[ 5]*y + matKKBtoA[ 6]*z + matKKBtoA[ 7];
-        w  = matKKBtoA[12]*x + matKKBtoA[13]*y + matKKBtoA[14]*z + matKKBtoA[15];
+        # Iterate over point sample in B
+        xB = xyzB[i*3+0];
+        yB = xyzB[i*3+1];
+        zB = xyzB[i*3+2];
+
+        # Iterate over point sample in B
+        nxB = normB[i*3+0];
+        nyB = normB[i*3+1];
+        nzB = normB[i*3+2];
+
+        # For each point in B, compute the projected coordinates in A
+        uw = matKK[ 0]*xB + matKK[ 1]*yB + matKK[ 2]*zB + matKK[ 3];
+        vw = matKK[ 4]*xB + matKK[ 5]*yB + matKK[ 6]*zB + matKK[ 7];
+        w  = matKK[12]*xB + matKK[13]*yB + matKK[14]*zB + matKK[15];
 	
         # Check that the normals in camera space are negative
         #if (zz > 0) ^ (w >= 0): continue
@@ -64,48 +66,55 @@ cdef _c_fast_icp(np.float32_t *xyzA,
         u = <int> (uw * w)
         v = <int> (vw * w)
 
-        # Discard if outside the image
+        # Discard if coordinates lie outside the image
         if not 0 <= u < width: continue
         if not 0 <= v < height: continue
 
+        # Discard masked-out pixels
         if maskA[v*width + u] == 0: continue
 
-        xB = matB[ 0]*x + matB[ 1]*y + matB[ 2]*z + matB[ 3];
-        yB = matB[ 4]*x + matB[ 5]*y + matB[ 6]*z + matB[ 7];
-        zB = matB[ 8]*x + matB[ 9]*y + matB[10]*z + matB[11];
-
+        # Look up the point values in A
         xA = xyzA[(v*width + u)*3+0]
         yA = xyzA[(v*width + u)*3+1]
         zA = xyzA[(v*width + u)*3+2]
 
+        # Look up the normal vectors in A
         nxA = normA[(v*width + u)*3+0];
         nyA = normA[(v*width + u)*3+1];
         nzA = normA[(v*width + u)*3+2];
 
-        # The following is borrowed directly from
+        # Reject points if the normals don't match
+        normdot = nxA * nxB + nyA * nyB + nzA * nzB
+        if normdot < normthresh: continue
+
+        # The following is taken directly from
         # pr2_fasticp.cc provided by Szymon Rusinkiewicz
         # http://www.cs.princeton.edu/~smr/software/pr2/
         d = (xB - xA) * nxA + \
             (yB - yA) * nyA + \
             (zB - zA) * nzA;
 
-        # Diagnostic: output u,v to check overlap
-
+        # Reject outliers
+        #print d*d
         if d*d > dist2: continue
-        err += d*d
+        sse += d*d
         npairs += 1
 
-        #uv[_blank*6+0] = xA
-        #uv[_blank*6+1] = yA
-        #uv[_blank*6+2] = zA
-        #uv[_blank*6+3] = xB
-        #uv[_blank*6+4] = yB
-        #uv[_blank*6+5] = zB
+        # Diagnostic: output u,v to check overlap
+        #uv[i*6+0] = u
+        #uv[i*6+1] = v
+        uv[i*6+0] = xA
+        uv[i*6+1] = yA
+        uv[i*6+2] = zA
+        uv[i*6+3] = xB
+        uv[i*6+4] = yB
+        uv[i*6+5] = zB
 
         c[0] = yA*nzA - zA*nyA
         c[1] = zA*nxA - xA*nzA
         c[2] = xA*nyA - yA*nxA
 
+        # Accumulate 6x6 and 6x1 matrices
         B[0] += d * c[0];
         B[1] += d * c[1];
         B[2] += d * c[2];
@@ -134,27 +143,24 @@ cdef _c_fast_icp(np.float32_t *xyzA,
         A[4][5] += nyA * nzA
         A[5][5] += nzA * nzA
 
-    err /= npairs
-    return err, npairs
+    return sse, npairs
 
 
 def _fasticp(np.ndarray[np.float32_t, ndim=3, mode='c'] xyzA,
              np.ndarray[np.float32_t, ndim=3, mode='c'] normA,
              np.ndarray[np.uint8_t, ndim=2, mode='c'] maskA,
              np.ndarray[np.float32_t, ndim=2, mode='c'] xyzB,
-             np.ndarray[np.float32_t, ndim=2, mode='c'] matB,
-             np.ndarray[np.float32_t, ndim=2, mode='c'] matKKBtoA,
-             float rate, float dist2):
+             np.ndarray[np.float32_t, ndim=2, mode='c'] normB,
+             np.ndarray[np.float32_t, ndim=2, mode='c'] matKK,
+             float dist2, float normthresh):
     height, width = maskA.shape[0], maskA.shape[1]
     length = xyzB.shape[0]
     assert xyzA.shape[0] == normA.shape[0] == height, 'xyzA normA shape height'
     assert xyzA.shape[1] == normA.shape[1] == width, 'xyzA normA shape'
     assert xyzA.shape[2] == normA.shape[2] == xyzB.shape[1] == 3, 'xyz norm xyzB shape'
-    assert matKKBtoA.shape[0] == matKKBtoA.shape[1] == 4, 'matKKBtoA shape'
-    assert matB.shape[0] == matB.shape[1] == 4, 'matB shape'
+    assert matKK.shape[0] == matKK.shape[1] == 4, 'matKK shape'
 
-
-    # Build an intermediate value UV coordinates matrix
+    # Build an intermediate value UV coordinates matrix (for debugging)
     cdef np.ndarray[np.float32_t, ndim=2, mode='c'] uv = \
          np.zeros((length, 6), dtype='f')
 
@@ -164,20 +170,21 @@ def _fasticp(np.ndarray[np.float32_t, ndim=3, mode='c'] xyzA,
     cdef np.ndarray[np.float32_t, ndim=1, mode='c'] B = \
          np.zeros((6,), dtype='f')
 
-    err, npairs = _c_fast_icp(<np.float32_t *> xyzA.data,
+    sse, npairs = _c_fast_icp(<np.float32_t *> xyzA.data,
                               <np.float32_t *> normA.data,
                               <np.uint8_t *> maskA.data,
                               height, width,
                               <np.float32_t *> xyzB.data,
+                              <np.float32_t *> normB.data,
                               length,
                               <np.float32_t *> uv.data,
-                              <np.float32_t *> matB.data,
-                              <np.float32_t *> matKKBtoA.data,
-                              rate, dist2,
+                              <np.float32_t *> matKK.data,
+                              dist2, 
+                               normthresh,
                               <np.float32_t (*)[6]> A.data,
                               <np.float32_t *> B.data
                               )
 
-    return err, npairs, uv, A, B
+    return sse, npairs, uv, A, B
 
 
--- rtmodel/fasticp.py
+++ rtmodel/fasticp.py
@@ -5,6 +5,53 @@ from _fasticp import _fasticp
 import transformations
 
 
+def fast_icp_multi(range_images, point_models, dist=0.01, normthresh=0.7):
+    global A, B, uv, mask, npairs, matKK
+
+    # For now, we assume that the xyz/normal data for both the
+    # point models and the range images are already in the same
+    # coordinate space.
+    within_eps = lambda a, b: np.all(np.abs(a-b)) < 1e-5
+    for rimg,pm in zip(range_images, point_models):
+        assert within_eps(rimg.RT, pm.RT), "Point model and range image data \
+            are assumed to be in the same coordinate space."
+
+    # Initialize accumulators
+    A = np.zeros((6,6))
+    B = np.zeros((6,))
+    uv = []
+    sse = 0.
+    npairs = 0
+
+    # Accumulate for each pair of range images and point clouds
+    for rimg,pm in zip(range_images, point_models):
+        KK = np.linalg.inv(np.dot(rimg.camera.RT, rimg.camera.KK))
+        sse_, npairs_, uv_, A_, B_ = _fasticp(rimg.xyz,
+                                              rimg.normals,
+                                              rimg.mask.astype('u1'),
+                                              pm.xyz,
+                                              pm.normals,
+                                              np.ascontiguousarray(KK),
+                                              dist*dist, normthresh)
+        npairs += npairs_
+        sse += sse_
+        uv.append(uv_)
+        A += A_
+        B += B_
+
+    # Fill out bottom triangle
+    for i in range(6):
+        for j in range(i):
+            A[i,j] = A[j,i]
+
+    xvec = np.linalg.solve(A, B)
+    RT = np.eye(4, dtype='f')
+    RT[:3,:3] = transformations.euler_matrix(*-xvec[:3])[:3,:3]
+    RT[:3,3] = -xvec[3:]
+
+    return RT, sse, npairs, uv
+
+
 def fast_icp(range_image, point_model, rate=0.001, dist=0.01):
     """Returns ICP between a range image and a point model.
     The range_image is held still, an altered 'point_model' is returned
